f9773100d3ab4af7f11c1cf12c81b7f998cb860c
QuentinBisson <quentin@giantswarm.io>
2023-12-11 10:42:33 +0100

    Test fake error

--- internal/app/loki-multi-tenant-proxy/server.go
+++ internal/app/loki-multi-tenant-proxy/server.go
@@ -1,79 +1,14 @@
 package proxy
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
+	"errors"
+	"log"
 
-	"github.com/giantswarm/loki-multi-tenant-proxy/internal/pkg"
 	"github.com/urfave/cli/v2"
-	"go.uber.org/zap"
 )
 
 // Serve serves
 func Serve(c *cli.Context) error {
-	lokiServerURL, _ := url.Parse(c.String("loki-server"))
-	addr := fmt.Sprintf(":%d", c.Int("port"))
-	authConfigLocation := c.String("auth-config")
-	authConfig, _ := pkg.ParseConfig(&authConfigLocation)
-	authConfig.KeepOrgID = c.Bool("keep-orgid")
-
-	logLevel := c.String("log-level")
-	if logLevel == "" {
-		logLevel = "INFO"
-	}
-
-	var logger *zap.Logger
-	{
-		zapConfig := zap.NewProductionConfig()
-		level, err := zap.ParseAtomicLevel(logLevel)
-		if err != nil {
-			return err
-		}
-		zapConfig.Level = level
-
-		logger = zap.Must(zapConfig.Build())
-		defer logger.Sync()
-	}
-
-	errorLogger, err := zap.NewStdLogAt(logger, zap.ErrorLevel)
-	if err != nil {
-		logger.Error("Could not create standard logger", zap.Error(err))
-		return err
-	}
-
-	var reverseProxy *httputil.ReverseProxy
-	{
-		reverseProxy = &httputil.ReverseProxy{
-			Rewrite: func(r *httputil.ProxyRequest) {
-				r.SetURL(lokiServerURL)
-				r.Out.Host = lokiServerURL.Host
-				r.Out.Header.Set("X-Forwarded-Host", lokiServerURL.Host)
-				orgID := r.In.Context().Value(OrgIDKey)
-
-				if orgID != "" {
-					r.Out.Header.Set("X-Scope-OrgID", orgID.(string))
-				}
-			},
-			ErrorLog: errorLogger,
-		}
-	}
-
-	handlers := Logger(
-		BasicAuth(
-			ReverseLoki(reverseProxy),
-			authConfig,
-		),
-		logger,
-	)
-
-	http.HandleFunc("/", handlers)
-	server := &http.Server{Addr: addr, ErrorLog: errorLogger}
-	if err := server.ListenAndServe(); err != nil {
-		logger.Error("Loki multi tenant proxy could not start", zap.Error(err))
-		return err
-	}
-	logger.Info("Starting HTTP server", zap.String("addr", addr))
-	return nil
+	log.Print("Error is here")
+	return cli.Exit(errors.New("test fake error"), -1)
 }
